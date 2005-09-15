a47f13e4e79139cb543b4b0628a11bf5c962cc01
tosco <gcms@users.noreply.github.com>
2005-09-15 22:27:39 +0000

    Removed some dependencies of ui on http

--- httptest/src/ui/Main.java
+++ httptest/src/ui/Main.java
@@ -2,22 +2,28 @@ package ui;
 
 import javax.swing.SwingUtilities;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class Main {
-    public static void main(String[] args) {
-        System.setProperty("org.apache.commons.logging.Log",
-                "org.apache.commons.logging.impl.NoOpLog");
+	public static void main(String[] args) {
+		System.setProperty("org.apache.commons.logging.Log",
+				"org.apache.commons.logging.impl.NoOpLog");
 
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+		}
 
-    public static void createAndShowGUI() {
-        JFrame frame = new MainFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
+	public static void createAndShowGUI() {
+		JFrame frame = new MainFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 }
