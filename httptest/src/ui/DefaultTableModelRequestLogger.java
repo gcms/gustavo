682c3d05bb108c022317ecff2d05ed76db5ea4ae
tosco <tosco@d5628d96-2816-dc11-9f3d-00188b745eb1>
2005-10-16 05:59:35 +0000

    Changed invokeLater to invokeAndWait to avoid deadlock problems

--- httptest/src/ui/DefaultTableModelRequestLogger.java
+++ httptest/src/ui/DefaultTableModelRequestLogger.java
@@ -1,5 +1,7 @@
 package ui;
 
+import java.lang.reflect.InvocationTargetException;
+
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 
@@ -7,22 +9,32 @@ import base.Request;
 import base.RequestLogger;
 
 public class DefaultTableModelRequestLogger implements RequestLogger {
-    private DefaultTableModel model;
+	private DefaultTableModel model;
 
-    public DefaultTableModelRequestLogger(DefaultTableModel model) {
-        this.model = model;
-    }
+	public DefaultTableModelRequestLogger(DefaultTableModel model) {
+		this.model = model;
+	}
 
-    public boolean isEditable(int row, int col) {
-        return false;
-    }
+	public boolean isEditable(int row, int col) {
+		return false;
+	}
 
-    public void log(final Request req, final long start, final long end) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                model.addRow(new Object[] { req, new Long(req.getSize()),
-                        new Long(start == -1 ? -1 : (end - start)) });
-            }
-        });
-    }
+	public void log(final Request req, final long start, final long end) {
+		Runnable runnable = new Runnable() {
+			public void run() {
+				model.addRow(new Object[] { req, new Long(req.getSize()),
+						new Long(start == -1 ? -1 : (end - start)) });
+			}
+		};
+		
+		try {
+			SwingUtilities.invokeAndWait(runnable);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- httptest/src/ui/JLabelSpeedObserver.java
+++ httptest/src/ui/JLabelSpeedObserver.java
@@ -5,6 +5,7 @@ import javax.swing.SwingUtilities;
 
 import base.SpeedRequestLogger;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Observer;
 import java.util.Observable;
 
@@ -15,12 +16,22 @@ public class JLabelSpeedObserver extends JLabel implements Observer {
         /* TODO: separate speed metering from request logging */
         final SpeedRequestLogger s = (SpeedRequestLogger) o;
         
-        SwingUtilities.invokeLater(new Runnable() {
+        Runnable runnable = new Runnable() {
             public void run() {
                 setHorizontalAlignment(RIGHT);
 
                 setText("Taxa média: " + s.toString());
             }
-        });
+        };
+		
+		try {
+			SwingUtilities.invokeAndWait(runnable);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
