9667d9cf7c639f755709874cdb2fb6420b0711d0
tosco <gcms@users.noreply.github.com>
2005-10-28 02:14:32 +0000

    Added ImageCursor and StringImageCursor.
    TODO: make multiple mouse work with menus.

---
+++ sdg/ImageCursor.java
@@ -0,0 +1,58 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+
+public class ImageCursor implements Cursor {
+
+    private Image image;
+
+    private Point hotSpot;
+
+    private static Image loadImage(String filename) {
+        Image result = null;
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+        result = toolkit.getImage(filename);
+
+        return result;
+    }
+
+    public ImageCursor(String filename, Point hotSpot) {
+        this(loadImage(filename), hotSpot);
+    }
+
+    public ImageCursor(Image image, Point hotSpot) {
+        this.image = image;
+        this.hotSpot = hotSpot;
+    }
+
+    public ImageCursor(Image image) {
+        this(image, 0, 0);
+    }
+
+    public ImageCursor(String filename) {
+        this(loadImage(filename));
+    }
+
+    public ImageCursor(Image image, int x, int y) {
+        this(image, new Point(x, y));
+    }
+
+    public ImageCursor(String filename, int x, int y) {
+        this(loadImage(filename), x, y);
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public void drawCursor(Graphics g, int x, int y) {
+        g.drawImage(image, x + hotSpot.x, y + hotSpot.y, null);
+    }
+}
