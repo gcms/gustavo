c05b0e91c29b4a2a067e272ddefc4fba463fbc15
tosco <tosco@d5628d96-2816-dc11-9f3d-00188b745eb1>
2006-04-12 14:54:58 +0000

    Adicionado contingoneline.h prevendo processo de orientacao a objetos de oneline.c

---
+++ continggui/contingoneline.h
@@ -0,0 +1,29 @@
+#ifndef CONTING_ONE_LINE_H
+#define CONTING_ONE_LINE_H
+
+#include <glib-object.h>
+
+#define CONTING_TYPE_ONE_LINE			(conting_one_line_get_type())
+#define CONTING_ONE_LINE(o)				(G_TYPE_CHECK_INSTANCE_CAST((o), \
+			CONTING_TYPE_ONE_LINE, ContingOneLine))
+#define CONTING_ONE_LINE_CLASS(k)		(G_TYPE_CHECK_CLASS_CAST((k), \
+			CONTING_TYPE_ONE_LINE, ContingOneLineClass))
+#define CONTING_IS_ONE_LINE(o)			(G_TYPE_CHECK_INSTANCE_TYPE((o), \
+			CONTING_TYPE_ONE_LINE))
+#define CONTING_IS_ONE_LINE_CLASS		(G_TYPE_CHECK_CLASS_TYPE((k), \
+			CONTING_TYPE_ONE_LINE))
+#define CONTING_ONE_LINE_GET_CLASS(o)	(G_TYPE_INSTANCE_GET_CLASS((o), \
+			CONTING_TYPE_ONE_LINE, ContingOneLineClass))
+
+typedef struct ContingOneLine_ ContingOneLine;
+struct ContingOneLine_ {
+	GObject parent;
+};
+
+typedef struct ContingOneLineClass_ ContingOneLineClass;
+struct ContingOneLineClass_ {
+	GObjectClass parent;
+};
+
+
+#endif /* CONTING_ONE_LINE_H */
