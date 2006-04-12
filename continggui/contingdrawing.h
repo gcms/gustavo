fe22d99fb4b57f8dfa3ce2fd13d063d769a706b6
tosco <gcms@users.noreply.github.com>
2006-04-12 01:04:24 +0000

    Barras e ramos funcionando

--- continggui/contingdrawing.h
+++ continggui/contingdrawing.h
@@ -25,11 +25,24 @@ typedef struct ContingDrawingClass_ ContingDrawingClass;
 struct ContingDrawingClass_ {
     GObjectClass parent;
 
+	/*
 	void (*get_rectangle)(ContingDrawing *self, GdkRectangle *rect);	
-    void (*draw) (ContingDrawing *self, GdkDrawable *drawable,
-			GdkGC *gc, const GdkPoint *position);
+	*/
+    void (*draw) (ContingDrawing *self, GdkDrawable *drawable, GdkGC *gc,
+			const GdkPoint *base_position, const GdkPoint *cur_position);
     void (*draw_selected) (ContingDrawing *self, GdkDrawable *drawable,
 			GdkGC *gc, const GdkPoint *position);
+
+	void (*start_place)(ContingDrawing *self);
+
+	void (*place)(ContingDrawing *self, gint x, gint y,
+			ContingDrawing *other);
+
+	gboolean (*is_placed)(ContingDrawing *self);
+
+	gboolean (*answer)(ContingDrawing *self, gint x, gint y);
+
+	/*
 	gboolean (*violates)(ContingDrawing *self, ContingDrawing *other,
 			gint x, gint y);
 
@@ -39,7 +52,6 @@ struct ContingDrawingClass_ {
 	gboolean (*place_linked)(ContingDrawing *self, gint selfx, gint selfy,
 			ContingDrawing *other, gint otherx, gint othery);
     gboolean (*place) (ContingDrawing *self);
-	/*
     gboolean (*is_link_point)(ContingDrawing *self,
             const GdkPoint *drawing_pos, const GdkPoint *ev_pos);
     void (*select)(ContingDrawing *self);
@@ -59,13 +71,17 @@ void conting_drawing_get_points(ContingDrawing *self,
         GdkPoint **links_points, gint *n_links);
 */
 
+/*
 void conting_drawing_get_rectangle(ContingDrawing *self,
         GdkRectangle *rect);
+*/
 
 void conting_drawing_draw(ContingDrawing *self,
-        GdkDrawable *drawable, GdkGC *gc, const GdkPoint *coord);
+		GdkDrawable *drawable, GdkGC *gc,
+		const GdkPoint *base_position, const GdkPoint *cur_position);
 void conting_drawing_draw_selected(ContingDrawing *self,
 		GdkDrawable *drawable, GdkGC *gc, const GdkPoint *coord);
+/*
 gboolean conting_drawing_violates(ContingDrawing *self,
 		ContingDrawing *other, gint x, gint y);
 gboolean conting_drawing_can_link(ContingDrawing *self,
@@ -74,5 +90,16 @@ gboolean conting_drawing_place(ContingDrawing *self);
 gboolean conting_drawing_place_linked(ContingDrawing *self,
 		gint selfx, gint selfy,
 		ContingDrawing *other, gint otherx, gint othery);
+*/
+
+void conting_drawing_start_place(ContingDrawing *self);
+
+void conting_drawing_place(ContingDrawing *self, gint x, gint y,
+		ContingDrawing *other);
+
+gboolean conting_drawing_is_placed(ContingDrawing *self);
+
+gboolean conting_drawing_answer(ContingDrawing *self, gint x, gint y);
+
 
 #endif /* CONTING_DRAWING_H */
