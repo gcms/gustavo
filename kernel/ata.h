e6c4dfd61aaea037280af4474cf397da34a6a61e
tosco <gcms@users.noreply.github.com>
2006-07-27 23:53:50 +0000

    Changes in the ata driver

--- kernel/ata.h
+++ kernel/ata.h
@@ -1,8 +1,23 @@
 #ifndef __ATA_H__
 #define __ATA_H__
 
-void hd_set_device(int device);
-void hd_read_sector(unsigned long sector, void *data);
-void hd_write_sector(unsigned long sector, void *data);
+
+/* CONTROLLERS */
+#define PRIMARY     0
+#define SECONDARY   1
+
+/* DEVICES */
+#define MASTER      0
+#define SLAVE       1
+
+
+void ata_init(void);
+void ata_read_sectors(unsigned char controller, unsigned char device,
+        unsigned long sector, unsigned char sector_count, void *data);
+void ata_write_sectors(unsigned char controller, unsigned char device,
+        unsigned long sector, unsigned char sector_count, void *data);
+
+#define ata_read_sector(c, d, s, b) ata_read_sectors(c, d, s, 1, b)
+#define ata_write_sector(c, d, s, b) ata_write_sectors(c, d, s, 1, b)
 
 #endif /* __ATA_H__ */
