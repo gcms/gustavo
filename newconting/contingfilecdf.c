64d3fd31383a9b058b2fb30af43be71030000b6e
tosco <gcms@users.noreply.github.com>
2006-08-28 13:24:49 +0000

    Had forgot to add contingfilecdf.?

---
+++ newconting/contingfilecdf.c
@@ -0,0 +1,155 @@
+#include "contingfile.h"
+#include "contingfilecdf.h"
+#include "contingitemdata.h"
+
+#include <stdio.h>
+#include <string.h>
+
+static ContingItemData *
+conting_file_cdf_read_bus(const char *line)
+{
+	GObject *item_data;
+	gint number, type;
+	gchar *name;
+
+	item_data = g_object_new(CONTING_TYPE_ITEM_DATA,
+			"type", CONTING_ITEM_TYPE_BUS,
+			NULL);
+
+	number = conting_file_int(line, 0, 3);
+	name = conting_file_string(line, 5, 16);
+	type = conting_file_int(line, 24, 25);
+
+	conting_item_data_set_attr(CONTING_ITEM_DATA(item_data),
+			"number", G_TYPE_INT, number,
+			"type", G_TYPE_INT, type,
+			"name", G_TYPE_STRING, name,
+			NULL);
+
+	g_free(name);
+
+	return CONTING_ITEM_DATA(item_data);
+}
+
+static ContingItemData *
+conting_file_cdf_read_branch(const char *line)
+{
+	GObject *item_data;
+
+	item_data = g_object_new(CONTING_TYPE_ITEM_DATA,
+			"type", CONTING_ITEM_TYPE_BRANCH,
+			NULL);
+
+	conting_item_data_set_attr(CONTING_ITEM_DATA(item_data),
+			"tap bus number", G_TYPE_INT, conting_file_int(line, 0, 3),
+			"z bus number", G_TYPE_INT, conting_file_int(line, 5, 8),
+			NULL);
+
+	return CONTING_ITEM_DATA(item_data);
+}
+
+static GList *
+conting_file_cdf_get_item_data(ContingFile *self, const gchar *filename)
+{
+	FILE *fp;
+	char buf[256];
+	GList *item_data_list;
+	ContingItemData *item_data;
+
+	g_print("get item data\n");
+	g_return_val_if_fail(self != NULL && CONTING_IS_FILE_CDF(self), NULL);
+
+
+	item_data_list = NULL;
+
+	fp = fopen(filename, "r");
+	
+	printf("fp = %p\n", fp);
+	if (fp == NULL)
+		return NULL;
+
+	while (fgets(buf, 256, fp)) {
+		if (strncmp(buf, "BUS", 3) == 0) {
+			while (fgets(buf, 256, fp)) {
+				if (strncmp(buf, "-999", 4) == 0)
+					break;
+
+				item_data = conting_file_cdf_read_bus(buf);
+				printf("item_data = %p\n", item_data);
+				if (item_data)
+					item_data_list = g_list_append(item_data_list, item_data);
+			}
+		} else if (strncmp(buf, "BRANCH", 6) == 0) {
+			while (fgets(buf, 256, fp)) {
+				if (strncmp(buf, "-999", 4) == 0)
+					break;
+
+				item_data = conting_file_cdf_read_branch(buf);
+				if (item_data)
+					item_data_list = g_list_append(item_data_list, item_data);
+			}
+		}
+	}
+
+	return item_data_list;
+}
+
+static void
+conting_file_cdf_file_init(gpointer g_iface, gpointer iface_data)
+{
+	ContingFileClass *file_class;
+
+	g_print("BEFORE");
+	file_class = g_iface;
+	file_class->get_item_data = conting_file_cdf_get_item_data;
+	g_print("AFTER");
+}
+
+static void
+conting_file_cdf_init(GTypeInstance *self, gpointer g_class)
+{
+}
+
+GType
+conting_file_cdf_get_type(void)
+{
+	static GType type = 0;
+
+	if (type == 0) {
+		static const GTypeInfo type_info = {
+			sizeof(ContingFileCDFClass),
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			sizeof(ContingFileCDF),
+			0,
+			conting_file_cdf_init
+		};
+
+		static const GInterfaceInfo iface_info = {
+			conting_file_cdf_file_init,
+			NULL,
+			NULL
+		};
+
+		type = g_type_register_static(G_TYPE_OBJECT,
+				"ContingFileCDF",
+				&type_info, 0);
+
+		g_type_add_interface_static(type,
+				CONTING_TYPE_FILE,
+				&iface_info);
+	}
+
+	return type;
+}
+
+ContingFile *
+conting_file_cdf_new(void)
+{
+	GObject *self = g_object_new(CONTING_TYPE_FILE_CDF, NULL);
+
+	return CONTING_FILE(self);
+}
