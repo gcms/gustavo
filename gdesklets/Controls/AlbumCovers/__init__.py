b46d66e1ba689fd5733aeae4e288ba9c3269b0e6
gustavo <gustavo@d5628d96-2816-dc11-9f3d-00188b745eb1>
2007-01-11 17:19:50 +0000

    AlbumCovers initial import.

---
+++ gdesklets/Controls/AlbumCovers/__init__.py
@@ -0,0 +1,75 @@
+from libdesklets.controls import Control, Interface, Permission
+import thread, threading
+import cover
+
+class IAlbumCovers(Interface):
+	artist		= Permission.WRITE
+	album		= Permission.WRITE
+
+	ready		= Permission.READWRITE
+
+	small		= Permission.READ
+	medium		= Permission.READ
+	large		= Permission.READ
+
+
+class AudioScrobblerCovers(Control, IAlbumCovers):
+	def __init__(self):
+		self._artist = None
+		self._album = None
+		self._ready = True
+		self._small = ""
+		self._medium = ""
+		self._large = ""
+
+		Control.__init__(self)
+
+
+	
+	def __set_artist(self, value):	self._artist = value
+	def __set_album(self, value):	self._album = value
+
+	def __set_ready(self, value):
+		while not self._ready:
+			pass
+		self._ready = False
+
+		thread.start_new_thread(self._get_info, ())
+
+	def __get_ready(self):
+		return self._ready
+
+	def __get_small(self):			return self._small
+	def __get_medium(self):			return self._medium
+	def __get_large(self):			return self._large
+
+	def _get_info(self):
+		print "_get_info()"
+		result = {}
+		try:
+			global result
+			result = cover.getCoverData(self._artist, self._album)
+		except Exception, inst:
+			print inst
+
+		print "result =", result
+		self._small = result.get('small')
+		self._medium = result.get('medium')
+		self._large = result.get('large')
+		print "medium = %s" % str(self._medium)
+
+		self._ready = True
+		self._update("ready")
+
+
+	artist = property(fset = __set_artist)
+	album  = property(fset = __set_album)
+
+	ready  = property(fget = __get_ready, fset = __set_ready)
+
+	small  = property(fget = __get_small)
+	medium = property(fget = __get_medium)
+	large  = property(fget = __get_large)
+
+def get_class(): return AudioScrobblerCovers
+
