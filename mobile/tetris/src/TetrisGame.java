b4c596c06063a2cecc8f97d04f9069648ac213f1
gcms <gcms@users.noreply.github.com>
2008-04-14 00:02:22 +0000

    Added file missing on the last commit.

---
+++ mobile/tetris/src/TetrisGame.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+
+public class TetrisGame {
+    private TetrisBoard board;
+
+    private int level;
+
+    public TetrisGame(int level) {
+        board = new TetrisBoard(10, 20);
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public TetrisBoard getBoard() {
+        return board;
+    }
+
+    public int getScore() {
+        return getBoard().getScore();
+    }
+
+    public TetrisGame(byte[] data) throws IOException {
+        this.level = 4; // should be in data
+        board = new TetrisBoard(data);
+    }
+
+    public byte[] getState() throws IOException {
+        return board.getState();
+    }
+}
